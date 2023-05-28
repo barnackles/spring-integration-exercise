c60ef5ccaff961fb63bfc39ec319b5a64328924f
barnackles <p.martewicz@yahoo.com>
2023-05-28 19:28:31 +0200

    Modify transformer to save fileName.

--- src/main/java/com/kodilla/integration/FileTransformer.java
+++ src/main/java/com/kodilla/integration/FileTransformer.java
@@ -1,21 +1,11 @@
 package com.kodilla.integration;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class FileTransformer {
 
     public String transformFile(String fileName) throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get(fileName));
-        StringBuilder result = new StringBuilder();
-
-        for (int n = lines.size() - 1; n >= 0; n--) {
-            result.append(lines.get(n)).append("\n");
-        }
-
-        return result.toString();
+        return fileName;
     }
 
 }
